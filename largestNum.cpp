36a67dc29ccedb6c60a5158d02ede1794c710533
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-04 15:28:11 +0530

    largest num

---
+++ largestNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 70;
+    int b = 15;
+
+    if(a>b) {
+        cout<< "Largest is a";
+    } else {
+        cout<< "Largest is b";
+    }
+}
