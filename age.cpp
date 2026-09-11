2255e25cd7c902a3195683c507f8a39fb4095b72
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-11 23:05:33 +0530

    class pro

--- age.cpp
+++ age.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
 using namespace std;
- 
-int main(){
-    int age = 25;
-    char grade='A';
-     float PI = 3.14f;
-    cout <<grade;
-    cout <<age;
-    cout <<PI;
-    cout<<sizeof(age);
-    return 0;
+class customer
+{
+    string name;
+    int  account_number,balance;
+    public:
+    customer(string name, int account_number,int balance)
+    {
+        this->name=name;
+        this->account_number=account_number;
+        this->account_number=balance;
+    }
+    void display()
+    {
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;
+    }
+};
+
+int main()
+{ 
+    // customer A1 A2();
+    customer A1("Rohit",12,2000);
+    customer A2("mohan",13,3000);
+    A1.display();
+    A2.display();
 }
