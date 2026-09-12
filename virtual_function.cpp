1762ba88b2d08bb89e5127375a51ad4bf2794637
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-12 23:05:45 +0530

    same changes

--- virtual_function.cpp
+++ virtual_function.cpp
@@ -1,27 +1,58 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// class Abhinandan
+// {
+//     public:
+//     virtual int show()
+//     {
+//         cout<<"This is Abhinandan class";
+//     }
+// };
+
+// class Verma: public Abhinandan
+// {
+//     public:
+//     int show()
+//     {
+//         cout<<"This is Verma class";
+//     }
+// };    
+// int main()
+// {
+// Abhinandan*ptr;
+// Verma obj;
+// ptr =&obj;
+// ptr ->show();
+
+// }  
+  
+
+// _____________________________________without virtual function_______________________________________-
+
+#include <iostream>
 using namespace std;
-class Abhinandan
-{
-    public:
-    int show()
-    {
-        cout<<"This is Abhinandan class";
+
+class Animal {
+public:
+        void sound() {
+        cout << "Animal makes a sound";
     }
 };
 
-class Verma: public Abhinandan
-{
-    public:
-    int show()
-    {
-        cout<<"This is Verma class";
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks";
     }
-};    
-int main()
-{
-Abhinandan*ptr;
-Verma obj;
-ptr =&obj;
-ptr ->show();
+};
 
-}  
+int main() {
+    Animal *a;
+    Dog d;
+
+    a = &d;
+
+    a->sound();
+
+    return 0;
+}
