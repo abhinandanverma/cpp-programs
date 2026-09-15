faf8cb67aa168e156be4ec8a51fc031ba8f71c61
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-15 18:10:30 +0530

    add comments

--- object_pasing_with_argument.cpp
+++ object_pasing_with_argument.cpp
@@ -8,15 +8,16 @@ class number
         {
             n=x;
         }
-        void add(number obj)
+        void add(number obj) // ek object perameter hai
         {
-            cout<<"sum"<<" "<<n+obj.n;
+            cout<<"sum"<<" "<<n+obj.n; // A ka n + B ke n ki value  
         }
 };
 int main()
 {
-    number A,B;
-    A.setData(10);
-    B.setData(20);
-    A.add(B);
+    number A,B; // Do object  banaya A or B.
+    A.setData(10); //A me 10 store hua.
+    B.setData(20); //B me 20 store hua.
+    A.add(B); // object B ko add()function ke argumet ke roop mein pass liya.
 }
+// Result =30 
