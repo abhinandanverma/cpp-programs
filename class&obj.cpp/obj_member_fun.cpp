3d1557f2a2cfd815ce3e8f2ba84720174dabe926
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-19 04:32:13 +0530

    member fun

---
+++ class&obj.cpp/obj_member_fun.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class customer
+{
+    
+    string name;
+    int account_number;
+    int balance;
+    public:
+    customer(string a,int b,int c){
+        name=a;
+        account_number=b;
+        balance=c;
+    }
+    void display(){
+        cout<<"Name :"<<name<<" account_number :"<<account_number<<" balancce : "<<balance<<endl;
+    }
+
+};
+int main(){
+    customer A1("Abhinandan",123,1000);
+    A1.display();
+}
