2c2d7983987cf6c3bc312e26f3ad0be74514da3e
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-18 19:47:05 +0530

    sum of the number

--- class&obj.cpp/functionc/add2num.cpp
+++ class&obj.cpp/functionc/add2num.cpp
@@ -17,20 +17,30 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// int add(int ,int);
+
+// int main(){
+//     int num1,num2;
+//     cin>>num1;
+//     cout<<endl;
+//     cin>>num2;
+//     cout<<add(num1 ,num2);
+// }
+
+
+//    int add (int a,int b){
+//    int sum=a+b;
+//    return sum;
+// }
+
+
 #include<iostream>
 using namespace std;
-int add(int a,int b );
-
-int main(){
-    int num1,num2;
-    cin>>num1;
-    cout<<endl;
-    cin>>num2;
-    cout<<add(num1 ,num2);
-}
-
-
-   int add (int a,int b){
-   int sum=a+b;
-   return sum;
-}
+ void printhello(){
+    cout<<"hello";
+ }
+ int main(){
+    printhello();
+ }
