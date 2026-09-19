ff69b49aea03b46e9167bbcd55ba2155656f8cbe
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-19 22:57:37 +0530

    public class

--- class&obj.cpp/functionc/callByRefe.cpp
+++ class&obj.cpp/functionc/callByRefe.cpp
@@ -1,32 +1,15 @@
-// #include<iostream>
-// using namespace std;
-// void swapPoniter(int* a,int* b){
-//     int temp = *a; // 4  4   5
-//     *a = *b;       // 4  5   5
-//     *b = temp;     // 4  5   4 
-// }
-// int main(){
-//     int x=4;
-//     int y=5;
-//     cout<<x<<" "<<y<<" "<<endl;
-//     swapPoniter(&x,&y);
-//     cout<<x<<" "<<y<<" "<<endl;
-//     return 0;
-// }
-
-//___________________________________using reference(&)______________________________-
 #include<iostream>
 using namespace std;
-void swapPoniter(int &a,int &b){
-    int temp = a; // 4  4   5
-    a = b;       // 4  5   5
-    b = temp;     // 4  5   4 
+void swapPoniter(int* a,int* b){
+    int temp = *a; // 4  4   5
+    *a = *b;       // 4  5   5
+    *b = temp;     // 4  5   4 
 }
 int main(){
     int x=4;
     int y=5;
     cout<<x<<" "<<y<<" "<<endl;
-    swapPoniter(x,y);
+    swapPoniter(&x,&y);
     cout<<x<<" "<<y<<" "<<endl;
     return 0;
 }
