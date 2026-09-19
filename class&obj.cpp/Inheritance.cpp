13a81f0dcfc27abc17cc835325902eb975bf03d2
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-20 02:21:11 +0530

    same changes

--- class&obj.cpp/Inheritance.cpp
+++ class&obj.cpp/Inheritance.cpp
@@ -4,12 +4,12 @@ class person{
     public:
     string name;
     int age;
-    person(string name,int age){
-        this->name=name;
-        this->age=age;
-    }
+    // person(string name,int age){
+    //     this->name=name;
+    //     this->age=age;
+    // }
     person(){
-        
+
     }
     
 };
@@ -19,14 +19,14 @@ class student : public person {
 
            void display()
            {
-              cout<< " name " <<name << " age " << age << " roll_number " << roll_number <<endl;
+              cout<< " Name: " <<name << " Age: " << age << " Roll_number: " << roll_number <<endl;
            }    
     };
 
     int main ()
     {
         student s1;
-        s1.name="abhinandan";
+        s1.name="Abhinandan";
         s1.age=19;
         s1.roll_number=03;
         s1.display();
