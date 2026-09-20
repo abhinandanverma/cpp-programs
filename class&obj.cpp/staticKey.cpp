bea32f7afb9432464b09d823a429e1f97b9241d5
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-21 02:59:20 +0530

    using a static key word

---
+++ class&obj.cpp/staticKey.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// void fun(){
+//      static  int x =0;  ///using aa static key word.
+//     cout << "x = " << x <<endl;
+//     x++;
+// }
+// int main(){
+//     fun();
+//     fun();
+//     fun();
+// }
+
+// _______________________________wothout static key word______________________
+
+
+#include<iostream>
+using namespace std;
+void fun(){
+    int x =0;
+    cout << "x = " << x <<endl;
+    x++;
+}
+int main(){
+    fun();
+    fun();
+    fun();
+}
